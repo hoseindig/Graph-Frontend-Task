1a8ccaedf5363866b5d8e07ec9f58cb5e5af77f7
hoseinx@gmail.com <hoseinx@gmail.com>
2026-02-09 12:13:26 +0330

    base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+        Test Tailwind
+      </button>
+    </>
+  );
+}
+
+export default App;
