2ce1b35d5342fbe84f4aa0e60edb09d960b5de0f
hoseinx@gmail.com <hoseinx@gmail.com>
2026-02-09 13:06:30 +0330

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,22 @@
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
-import CustomModal from "./components/CustomModal";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClose = () => setIsOpen(false);
-  const handleDelete = () => {
-    console.log("Item deleted");
-    setIsOpen(false);
-  };
   return (
-    <>
-      <button
-        onClick={() => setIsOpen(true)}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-      >
-        Show Modal
-      </button>
-      <CustomModal
-        isOpen={isOpen}
-        onClose={handleClose}
-        title="Delete Confirmation"
-        confirmText="Delete"
-        onConfirm={handleDelete}
-        animationDuration={200} // 500ms animation
-        animateBackdrop={true} // Fade backdrop
-        animateModal={true} // Fade modal
-        showBackdrop={true} // Show backdrop
-        closeOnBackdropClick={true} // Click backdrop to close
-      >
-        <p>Are you sure you want to delete this item?</p>
-      </CustomModal>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
