dbcf392850799b6246c812ea02969dfb1586c8ac
hoseinx@gmail.com <hoseinx@gmail.com>
2026-02-11 07:04:08 +0330

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        dash: {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "12px 0" },
+        },
+      },
+      animation: {
+        dash: "dash 1.5s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
